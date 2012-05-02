756f29051c3363be751bc3b8928611dc0d2364bc
Chris Patterson <chris@phatboyg.com>
2012-05-02 13:29:28 -0500

    Added test for TimeSpan extension

---
+++ src/Internals.Tests/Extensions/TimeSpan_Specs.cs
@@ -0,0 +1,20 @@
+﻿namespace Internals.Tests.Extensions
+{
+    using System;
+    using NUnit.Framework;
+    using Internals.Extensions;
+
+    [TestFixture]
+    public class TimeSpan_Specs
+    {
+        [Test]
+        public void Should_property_format_a_timespan()
+        {
+            TimeSpan span = new TimeSpan(1, 2, 3, 4);
+
+            string friendly = span.ToFriendlyString();
+
+            Assert.AreEqual("1d2h3m4s", friendly);
+        }
+    }
+}
