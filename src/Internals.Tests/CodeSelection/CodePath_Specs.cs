e45c3c6e715d71db689089dba3e6a0f9b2c9c330
Chris Patterson <chris@phatboyg.com>
2012-09-05 18:07:18 -0500

    Updated tests to include conditional code path builder

---
+++ src/Internals.Tests/CodeSelection/CodePath_Specs.cs
@@ -0,0 +1,73 @@
+﻿namespace Internals.Tests.CodeSelection
+{
+    using System;
+    using System.Collections.Generic;
+    using Conditional;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class When_a_configurable_code_path_is_used
+    {
+        [Test]
+        public void Should_default_to_the_default_code_path()
+        {
+            CodePath<DebugMode>.If(DefaultPath, PreferredPath);
+
+            int rate = CodePath<DebugMode>.Iff(DefaultRate, PreferredRate, 10);
+
+            // forcing it to enabled
+            CodePath<DebugMode>.Enable();
+
+            // forcing it to disabled
+            CodePath<DebugMode>.Disable();
+
+            CodePath<DebugMode>.Unless(() => { });
+
+            // setting it used a custom type
+//            CodePath<DebugMode>.Set(new EnvironmentMode("DebugMode"));
+
+
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+
+            string text;
+            bool result = CodePath<DebugMode>.Iff(dictionary.TryGetValue, dictionary.TryGetValue, 27, out text);
+
+        }
+
+
+        void DefaultPath()
+        {
+        }
+
+        void PreferredPath()
+        {
+        }
+
+        int DefaultRate(int input)
+        {
+            return input*10;
+        }
+
+        int PreferredRate(int input)
+        {
+            return input*15;
+        }
+    }
+
+    class DebugMode :
+        Mode
+    {
+        public bool Enabled
+        {
+            get
+            {
+#if DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+    }
+
+}
