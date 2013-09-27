3e8279bd7be2c8529a2ef1a7098a5a677b926284
Chris Patterson <chris@phatboyg.com>
2013-09-27 09:48:52 -0700

    Added mapping tests

---
+++ src/Internals.Tests/Reflection/DictionaryToObject_Specs.cs
@@ -0,0 +1,147 @@
+﻿namespace Internals.Tests.Reflection
+{
+    using System.Collections.Generic;
+    using Internals.Reflection;
+    using Mapping;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class Converting_a_dictionary_to_an_object
+    {
+        [Test]
+        public void Should_include_a_string()
+        {
+            Assert.IsNotNullOrEmpty(_values.StringValue);
+            Assert.AreEqual("Hello", _values.StringValue);
+        }
+
+        [Test]
+        public void Should_include_nullable_long()
+        {
+            Assert.IsTrue(_values.LongValue.HasValue);
+            Assert.AreEqual(123, _values.LongValue.Value);
+        }
+
+        [Test]
+        public void Should_include_the_enum()
+        {
+            Assert.AreEqual(ValueType.Integer, _values.ValueType);
+        }
+
+        [Test]
+        public void Should_include_the_enum_as_a_string()
+        {
+            Assert.AreEqual(ValueType.String, _values.ValueTypeAsString);
+        }
+
+        [Test]
+        public void Should_include_the_enum_as_an_integer()
+        {
+            Assert.AreEqual(ValueType.Integer, _values.ValueTypeAsInt);
+        }
+
+        [Test]
+        public void Should_include_the_integer()
+        {
+            Assert.AreEqual(27, _values.IntValue);
+        }
+
+        [Test]
+        public void Should_include_the_sub_value()
+        {
+            Assert.IsNotNull(_values.SubValue);
+
+            Assert.AreEqual("A", _values.SubValue.A);
+            Assert.AreEqual("B", _values.SubValue.B);
+        }
+
+        [Test]
+        public void Should_include_the_sub_values()
+        {
+            Assert.IsNotNull(_values.SubValues);
+            Assert.AreEqual(2, _values.SubValues.Length);
+
+            Assert.AreEqual("A", _values.SubValues[0].A);
+            Assert.AreEqual("B", _values.SubValues[0].B);
+
+            Assert.AreEqual("1", _values.SubValues[1].A);
+            Assert.AreEqual("2", _values.SubValues[1].B);
+        }
+
+        IDictionary<string, object> _dictionary;
+        Values _values;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            _dictionary = new Dictionary<string, object>
+                {
+                    {"IntValue", 27},
+                    {"StringValue", "Hello"},
+                    {"LongValue", (long?)123},
+                    {"ValueType", ValueType.Integer},
+                    {"ValueTypeAsInt", 2},
+                    {"ValueTypeAsString", "String"},
+                    {
+                        "SubValue", new Dictionary<string, object>
+                            {
+                                {"A", "A"},
+                                {"B", "B"}
+                            }
+                    },
+                    {"StringValues", new[] {"A", "B", "C"}},
+                    {
+                        "SubValues", new object[]
+                            {
+                                new Dictionary<string, object>
+                                    {
+                                        {"A", "A"},
+                                        {"B", "B"}
+                                    },
+                                new Dictionary<string, object>
+                                    {
+                                        {"A", "1"},
+                                        {"B", "2"}
+                                    }
+                            }
+                    },
+                };
+
+
+            ObjectConverterCache converterCache =
+                new DynamicObjectConverterCache(new DynamicImplementationBuilder());
+
+            _values = (Values)converterCache.GetConverter(typeof(Values)).GetObject(_dictionary);
+        }
+
+
+        public interface Values
+        {
+            int IntValue { get; }
+            string StringValue { get; }
+            long? LongValue { get; }
+            ValueType ValueType { get; }
+            ValueType ValueTypeAsInt { get; }
+            ValueType ValueTypeAsString { get; }
+            SubValue SubValue { get; }
+            string[] StringValues { get; }
+            SubValue[] SubValues { get; }
+        }
+
+
+        public interface SubValue
+        {
+            string A { get; }
+            string B { get; }
+        }
+
+
+        public enum ValueType
+        {
+            Default,
+            String,
+            Integer
+        }
+    }
+}
---
+++ src/Internals.Tests/Reflection/ObjectToDictionary_Specs.cs
@@ -0,0 +1,174 @@
+﻿namespace Internals.Tests.Reflection
+{
+    using System;
+    using System.Collections.Generic;
+    using Mapping;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class Creating_a_dictionary_from_an_interface
+    {
+        [Test]
+        public void Should_handle_a_present_nullable_value()
+        {
+            Assert.IsTrue(_dictionary.ContainsKey("NullableDecimalValue"));
+        }
+
+        [Test]
+        public void Should_handle_a_string()
+        {
+            Assert.IsTrue(_dictionary.ContainsKey("StringValue"));
+        }
+
+        [Test]
+        public void Should_handle_an_array_of_objects()
+        {
+            Assert.IsTrue(_dictionary.ContainsKey("SubValues"));
+
+            object value = _dictionary["SubValues"];
+
+            Assert.IsInstanceOf<IDictionary<string, object>[]>(value);
+        }
+
+        [Test]
+        public void Should_handle_an_array_of_strings()
+        {
+            Assert.IsTrue(_dictionary.ContainsKey("Names"));
+
+            object value = _dictionary["Names"];
+
+            Assert.IsInstanceOf<string[]>(value);
+        }
+
+        [Test]
+        public void Should_handle_an_empty_nullable_value()
+        {
+            Assert.IsFalse(_dictionary.ContainsKey("NullableValue"));
+        }
+
+        [Test]
+        public void Should_handle_an_int()
+        {
+            Assert.IsTrue(_dictionary.ContainsKey("IntValue"));
+        }
+
+        [Test]
+        public void Should_handle_datetime()
+        {
+            Assert.IsTrue(_dictionary.ContainsKey("DateTimeValue"));
+        }
+
+        IDictionary<string, object> _dictionary;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            var factory = new DictionaryConverterCache();
+
+            DictionaryConverter converter = factory.GetConverter(typeof(ValuesImpl));
+
+            _dictionary =
+                converter.GetDictionary(new ValuesImpl("Hello", 27, new DateTime(2012, 10, 1), null, 123.45m));
+        }
+
+
+        interface Values
+        {
+            string StringValue { get; }
+            int IntValue { get; }
+            DateTime DateTimeValue { get; }
+            long? NullableValue { get; }
+            decimal? NullableDecimalValue { get; }
+            string[] Names { get; }
+
+            SubValue[] SubValues { get; }
+        }
+
+
+        interface SubValue
+        {
+            string Text { get; }
+        }
+
+
+        class ValuesImpl :
+            Values
+        {
+            readonly DateTime _dateTimeValue;
+            readonly int _intValue;
+            readonly decimal? _nullableDecimalValue;
+            readonly long? _nullableValue;
+            readonly string _stringValue;
+            string[] _names;
+            SubValue[] _subValues;
+
+            public ValuesImpl(string stringValue, int intValue, DateTime dateTimeValue, long? nullableValue,
+                decimal? nullableDecimalValue)
+            {
+                _stringValue = stringValue;
+                _intValue = intValue;
+                _dateTimeValue = dateTimeValue;
+                _nullableValue = nullableValue;
+                _nullableDecimalValue = nullableDecimalValue;
+                _names = new[] {"A", "B", "C"};
+                _subValues = new SubValue[]
+                    {
+                        new SubValueImpl("A")
+                    };
+            }
+
+
+            public string StringValue
+            {
+                get { return _stringValue; }
+            }
+
+            public int IntValue
+            {
+                get { return _intValue; }
+            }
+
+            public DateTime DateTimeValue
+            {
+                get { return _dateTimeValue; }
+            }
+
+            public long? NullableValue
+            {
+                get { return _nullableValue; }
+            }
+
+            public decimal? NullableDecimalValue
+            {
+                get { return _nullableDecimalValue; }
+            }
+
+            public string[] Names
+            {
+                get { return _names; }
+            }
+
+            public SubValue[] SubValues
+            {
+                get { return _subValues; }
+            }
+
+
+            class SubValueImpl : SubValue
+            {
+                readonly string _text;
+
+                public SubValueImpl(string text)
+                {
+                    _text = text;
+                }
+
+                public string Text
+                {
+                    get { return _text; }
+                }
+            }
+        }
+    }
+}
